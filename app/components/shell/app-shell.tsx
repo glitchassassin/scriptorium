1d2beb7d5983812ad3f806091b72227f7d71921d
Jon Winsley <jon.winsley@gmail.com>
2026-03-13 17:34:03 -0400

    fix: align shell with visible viewport

--- app/components/shell/app-shell.tsx
+++ app/components/shell/app-shell.tsx
@@ -50,7 +50,7 @@ export function AppShell({ passkeyLabel }: AppShellProps) {
   }, [isSidebarOpen]);
 
   return (
-    <main className="relative h-screen bg-white px-6 text-black sm:px-8">
+    <main className="relative h-dvh bg-white px-6 text-black sm:px-8">
       {isSidebarOpen ? (
         <>
           <button
